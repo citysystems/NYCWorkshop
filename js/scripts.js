aeb1c710df1feafa4daa9945bd605ef44cc11e5e
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-05-06 15:34:44 -0400

    updated formatting and index calc (need to finalize index calc and score scaling)

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
-
 //Create the map variable
 var map = L.map('my-map', {
     scrollWheelZoom: false
@@ -15,14 +14,6 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 	maxZoom: 19
 }).addTo(map);
 
-function getColor(d) {
-  return d < 8 ? '#4FDE02' :
-      d < 16 ? '#A0EB15' :
-      d < 29 ? '#E9D00E' :
-      d < 38 ? '#E76607' :
-      '#EC0803';
-};
-console.log(distances[0]);
 function getColorAmen(d) {
   switch (d) {
     case 'store':
@@ -39,6 +30,9 @@ function getColorAmen(d) {
       return 'black';
     }
   };
+scores = [];
+maxScore = 0;
+minScore = 0;
 function onEachFeature(feature, layer) {
   //Get buttons
   var absoluteAmenW = [$("#storeVal"), $("#pharmaVal"), $("#bankVal"), $("#supermarketVal"),
@@ -50,12 +44,12 @@ function onEachFeature(feature, layer) {
   for (var i = 0; i < absoluteAmenW.length; i++) {
     absoluteAmenW[i].val(parseInt(absoluteAmenW[i].val()));
       if (absoluteAmenW[i].val() > 10 ) {
-          alert('Please choose values between 1 and 10! Value changed to 10 in index calculation.');
+          alert('Please choose values between 0 and 10! Value changed to 10 in index calculation.');
           absoluteAmenW[i].val(10);
       };
-      if (absoluteAmenW[i].val() < 1) {
-          alert('Please choose values between 1 and 10! Value changed to 1 in index calculation.');
-          absoluteAmenW[i].val(1);
+      if (absoluteAmenW[i].val() < 0) {
+          alert('Please choose values between 0 and 10! Value changed to 0 in index calculation.');
+          absoluteAmenW[i].val(0);
       };
     };
   for (var i = 0; i < marginalAmenW.length; i++) {
@@ -72,12 +66,12 @@ function onEachFeature(feature, layer) {
   for (var i = 0; i < absoluteTransitW.length; i++) {
     absoluteTransitW[i].val(parseInt(absoluteTransitW[i].val()));
       if (absoluteTransitW[i].val() > 10 ) {
-          alert('Please choose values between 1 and 10! Value changed to 10 in index calculation.');
+          alert('Please choose values between 0 and 10! Value changed to 10 in index calculation.');
           absoluteTransitW[i].val(10);
       };
-      if (absoluteTransitW[i].val() < 1) {
-          alert('Please choose values between 1 and 10! Value changed to 1 in index calculation.');
-          absoluteTransitW[i].val(1);
+      if (absoluteTransitW[i].val() < 0) {
+          alert('Please choose values between 0 and 10! Value changed to 1 in index calculation.');
+          absoluteTransitW[i].val(0);
       };
     };
   for (var i = 0; i < marginalTransitW.length; i++) {
@@ -91,18 +85,20 @@ function onEachFeature(feature, layer) {
           marginalTransitW[i].val(1);
       };
     };
-  var absoluteAmenW = [$("#storeVal").val(), $("#pharmaVal").val(), $("#bankVal").val(), $("#supermarketVal").val(),
-      $("#restaurantVal").val()];
+  var absoluteAmenW = [parseInt($("#storeVal").val()), parseInt($("#pharmaVal").val()), parseInt($("#bankVal").val()), parseInt($("#supermarketVal").val()),
+      parseInt($("#restaurantVal").val())];
   //marginal amenity calc: 1 - (1/(MarginalVal))
   var marginalAmenW = [(1-(1/$("#storeMVal").val())), (1-(1/$("#pharmaMVal").val())), (1-(1/$("#bankMVal").val())), (1-(1/$("#supermarketMVal").val())),
       (1-(1/$("#restaurantMVal").val()))];
-  var absoluteTransitW = [$("#walkVal").val(), $("#bikeVal").val(), $("#transitVal").val(), $("#driveVal").val()];
+  var absoluteTransitW = [parseInt($("#walkVal").val()), parseInt($("#bikeVal").val()), parseInt($("#transitVal").val()), parseInt($("#driveVal").val())];
   //marginal transit calc: -ln0.1/t(minutes)
-  var marginalTransitW = [((-Math.log(0.1))/($("#walkMVal").val())), ((-Math.log(0.1))/($("#bikeMVal").val())), ((-Math.log(0.1))/($("#transitMVal").val())),
-  ((-Math.log(0.1))/($("#transitMVal").val()))];
+  var marginalTransitW = [((Math.log(0.1))/($("#walkMVal").val())), ((Math.log(0.1))/($("#bikeMVal").val())), ((Math.log(0.1))/($("#transitMVal").val())),
+  ((Math.log(0.1))/($("#transitMVal").val()))];
   parcel = layer.feature.properties.ID;
-  console.log(parcel);
   score = 0;
+  //marginal good implementation = total transit score * MarginalAmenW(given type)^"rank"
+  //util per transit: exp(marginal weighting * actual travel time)
+  //marginal score per transit type, scale each according to absolute weights, sum together, marginal good weighting, scale by the absolute good weighting, add together
   for (var i = 0; i < distances.length; i++) {
     walkScore = 0;
     bikeScore = 0;
@@ -110,25 +106,31 @@ function onEachFeature(feature, layer) {
     driveScore = 0;
     if (distances[i].origin == parcel) {
       if (distances[i].type == 'restaurant') {
-        walkScore = walkScore + Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
-        bikeScore = bikeScore + Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
-        transitScore = transitScore + Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
-        driveScore = driveScore + Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
+        walkScore =  Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
+        // console.log(marginalTransitW[0]);
+        // console.log(distances[i]["Walk Time"]);
+        // console.log("Marginal Walk Score:" + walkScore);
+        bikeScore =  Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
+        transitScore =  Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
+        driveScore =  Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
         walkScore = walkScore * absoluteTransitW[0];
+        // console.log("Absolute Walk Score:" + walkScore);
         bikeScore = bikeScore * absoluteTransitW[1];
         transitScore = transitScore * absoluteTransitW[2];
         driveScore = driveScore * absoluteTransitW[3];
         aggScore = walkScore + bikeScore + transitScore + driveScore;
+        // console.log("Agg Score:" + aggScore);
         aggScore = aggScore * Math.pow(marginalAmenW[4],distances[i]["rank"]);
+        // console.log("Agg Score:" + aggScore);
         aggScore = aggScore * absoluteAmenW[4];
         score += aggScore;
-        console.log(score);
+        // console.log(score);
       }
       if (distances[i].type == 'bank') {
-        walkScore = walkScore + Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
-        bikeScore = bikeScore + Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
-        transitScore = transitScore + Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
-        driveScore = driveScore + Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
+        walkScore =  Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
+        bikeScore =  Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
+        transitScore =  Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
+        driveScore = Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
         walkScore = walkScore * absoluteTransitW[0];
         bikeScore = bikeScore * absoluteTransitW[1];
         transitScore = transitScore * absoluteTransitW[2];
@@ -137,13 +139,13 @@ function onEachFeature(feature, layer) {
         aggScore = aggScore * Math.pow(marginalAmenW[2],distances[i]["rank"]);
         aggScore = aggScore * absoluteAmenW[2];
         score += aggScore;
-        console.log(score);
+        // console.log(score);
       }
       if (distances[i].type == 'pharmacy') {
-        walkScore = walkScore + Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
-        bikeScore = bikeScore + Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
-        transitScore = transitScore + Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
-        driveScore = driveScore + Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
+        walkScore =  Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
+        bikeScore =  Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
+        transitScore =  Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
+        driveScore =  Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
         walkScore = walkScore * absoluteTransitW[0];
         bikeScore = bikeScore * absoluteTransitW[1];
         transitScore = transitScore * absoluteTransitW[2];
@@ -152,13 +154,13 @@ function onEachFeature(feature, layer) {
         aggScore = aggScore * Math.pow(marginalAmenW[1],distances[i]["rank"]);
         aggScore = aggScore * absoluteAmenW[1];
         score += aggScore;
-        console.log(score);
+        // console.log(score);
       }
       if (distances[i].type == 'store') {
-        walkScore = walkScore + Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
-        bikeScore = bikeScore + Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
-        transitScore = transitScore + Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
-        driveScore = driveScore + Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
+        walkScore =  Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
+        bikeScore =  Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
+        transitScore =  Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
+        driveScore =  Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
         walkScore = walkScore * absoluteTransitW[0];
         bikeScore = bikeScore * absoluteTransitW[1];
         transitScore = transitScore * absoluteTransitW[2];
@@ -167,13 +169,13 @@ function onEachFeature(feature, layer) {
         aggScore = aggScore * Math.pow(marginalAmenW[0],distances[i]["rank"]);
         aggScore = aggScore * absoluteAmenW[0];
         score += aggScore;
-        console.log(score);
+        // console.log(score);
       }
       if (distances[i].type == 'supermarket') {
-        walkScore = walkScore + Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
-        bikeScore = bikeScore + Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
-        transitScore = transitScore + Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
-        driveScore = driveScore + Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
+        walkScore =  Math.exp(marginalTransitW[0]*distances[i]["Walk Time"]);
+        bikeScore =  Math.exp(marginalTransitW[1]*distances[i]["Bike Time"]);
+        transitScore =  Math.exp(marginalTransitW[2]*distances[i]["Transit Time"]);
+        driveScore =  Math.exp(marginalTransitW[3]*distances[i]["Drive Time"]);
         walkScore = walkScore * absoluteTransitW[0];
         bikeScore = bikeScore * absoluteTransitW[1];
         transitScore = transitScore * absoluteTransitW[2];
@@ -182,38 +184,30 @@ function onEachFeature(feature, layer) {
         aggScore = aggScore * Math.pow(marginalAmenW[3],distances[i]["rank"]);
         aggScore = aggScore * absoluteAmenW[3];
         score += aggScore;
-        console.log(score);
+        // console.log(score);
       };
     };
   };
-    layer.feature.properties.score = score;
-  //marginal good implementation = total transit score * MarginalAmenW(given type)^"rank"
-  //util per transit: exp(marginal weighting * actual travel time)
-  //marginal score per transit type, scale each according to absolute weights, sum together, marginal good weighting, scale by the absolute good weighting, add together
-    if (layer.feature.properties && layer.feature.properties.score) {
-        layer.bindPopup("Accessibility Score: " + layer.feature.properties.score);
-    };
-    layer.setStyle({
-        fillColor: getColor(layer.feature.properties.score),
-        weight: 0,
-        opacity: 0,
-        color: "lightgrey",
-        // dashArray: '3',
-        fillOpacity: 1
-    });
+  layer.feature.properties.score = score;
+  if (layer.feature.properties && layer.feature.properties.score) {
+      layer.bindPopup("Accessibility Score: " + layer.feature.properties.score + ", Parcel: " + layer.feature.properties.ID);
+  };
+  layer.setStyle({
+      fillColor: getColor(layer.feature.properties.score),
+      weight: 1,
+      // dashArray: '3 10',
+      // opacity: 0,
+      color: "black",
+      // dashArray: '3',
+      fillOpacity: 0.75
+  });
+  if (score > maxScore) {
+    maxScore = score;
+  };
+  if (score < minScore) {
+    minScore = score;
+  }
 };
-
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-//
-var lots = L.geoJSON(parcels, {
-    onEachFeature: onEachFeature
-});
-
-
 
 var store = L.geoJson(amenities, {
   filter: function(feature, layer) {
@@ -288,14 +282,20 @@ var restaurant = L.geoJson(amenities, {
 var legend = L.control({
     position: 'topright'
 });
-
+function getColor(d) {
+  return d < 1 ? '#4FDE02' :
+      d < 5 ? '#A0EB15' :
+      d < 10 ? '#E9D00E' :
+      d < 20 ? '#E76607' :
+      '#EC0803';
+};
 legend.onAdd = function(map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [7,8,16,29,38],
-        labels = ['< 8 minutes', "8-16 minutes", '17-29 minutes',
-        '30-38 minutes', '> 38 minutes'];
-    div.innerHTML = '<div><b>Time from Services</b></div>';
+        grades = [0,1,9,19,40],
+        labels = ['Less than 1', "1-5", '5-10',
+        '10-20', '20+'];
+    div.innerHTML = '<div><b>Accessibility Score</b></div>';
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
@@ -307,6 +307,7 @@ legend.onAdd = function(map) {
 };
 
 legend.addTo(map);
+
 
 var legend1 = L.control({
     position: 'bottomleft'
@@ -332,7 +333,6 @@ legend1.addTo(map);
 
 
 var overlays = {
-    "Parcels": lots,
     'Store': store,
     "Pharmacy": pharmacy,
     "Bank": bank,
@@ -342,44 +342,47 @@ var overlays = {
 
 L.control.layers(null, overlays).addTo(map);
 
-// var bank
-//
-// var ST1=5;
-// var ST2=1;
-// var Walk=5;
-// var Bike=2;
-// var Transit=7;
-// var Drive=5;
-// var MarginalDestination=0.1;
-//
-// var features = parcels.features;
-//
-// var FC = {
-//     type: 'FeatureCollection',
-//     features: features,
-//   };
-// // filteredLayer.clearLayers();
-//   // create a Leaflet geojson layer from the FeatureCollection
-// var j = 0;
-// var parkFeatures = parks.features;
-// function updateScore(feature, layer) {
-//   feature.properties.POLY_CODE = 0;
-//   var score = 0;
-//   for (var i = 0; i < parkFeatures.length; i++) {
-//     var marginalDFactor = (1-(MarginalDestination*i))
-//     if (marginalDFactor > 0) {
-//         console.log(((bikeScores[i][j] * Bike) + (walkScores[i][j] * Walk) + (transitScores[i][j] * Transit) + (driveScores[i][j] * Drive)))
-//         score += (marginalDFactor*((bikeScores[i][j] * Bike) + (walkScores[i][j] * Walk) + (transitScores[i][j] * Transit) + (driveScores[i][j] * Drive)));
-//     } else {
-//         score = score
-//     }
-//         // score += MarginalDestination*((bikeScores[i][j] * Bike) + (walkScores[i][j] * Walk) + (transitScores[i][j] * Transit) + (driveScores[i][j] * Drive));
-//   };
-//   console.log(score*ST1);
-//   layer.feature.properties.POLY_CODE = score*ST1;
-//   j = j + 1;
-// };
-// filteredLayer = L.geoJSON(FC, {
-//   onEachFeature: updateScore,
-// });
-// console.log(filteredLayer);
+var filteredLayer = L.geoJSON(parcels, {
+  onEachFeature: onEachFeature,
+});
+
+function updateMap() {
+  maxScore = 0;
+  minScore = 0;
+  var features = parcels.features
+  var FC = {
+    type: 'FeatureCollection',
+    features: features,
+  };
+  filteredLayer.clearLayers();
+  // create a Leaflet geojson layer from the FeatureCollection
+  filteredLayer = L.geoJSON(FC, {
+    onEachFeature: onEachFeature,
+  });
+  // steps = (maxScore - minScore)/5
+  // function getColor(d) {
+  //   return d < minScore + steps ? '#4FDE02' :
+  //       d < minScore + 2*steps ? '#A0EB15' :
+  //       d < minScore + 3*steps ? '#E9D00E' :
+  //       d < minScore + 4*steps ? '#E76607' :
+  //       '#EC0803';
+  // };
+  // filteredLayer = L.geoJSON(FC, {
+  //   onEachFeature: onEachFeature,
+  // });
+  // filteredLayer.setStyle({
+  //   fillColor: getColor(filteredLayer.feature.properties.score),
+  //   weight: 1,
+  //   // dashArray: '3 10',
+  //   // opacity: 0,
+  //   color: "black",
+  //   // dashArray: '3',
+  //   fillOpacity: 0.75
+  // });
+  console.log(maxScore);
+  console.log(minScore);
+  filteredLayer.addTo(map);
+};
+//first map populated!
+updateMap();
+console.log(filteredLayer);
